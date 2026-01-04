71e9fda95fc5f97e86fb72382cf505352b4bf983
flamenir-a11y <flame.nir@gmail.com>
2026-01-04 23:05:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: "/-/",
 
     const env = loadEnv(mode, '.', '');
     return {
+        base: "/-/",
+
       server: {
         port: 3000,
         host: '0.0.0.0',
